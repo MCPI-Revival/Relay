ff8264a9099fc26f7729c189552eb23b8b1df716
NikZapp <58770289+NikZapp@users.noreply.github.com>
2023-12-09 00:26:03 +0600

    Remove old auth system part 1

--- MCPIRelay/server.cpp
+++ MCPIRelay/server.cpp
@@ -2,11 +2,6 @@
 #include <client.hpp>
 #include <json.hpp>
 
-#include <network/packets/server_info_packet.hpp>
-#include <network/packets/connection_status_packet.hpp>
-#include <network/packets/connection_request_packet.hpp>
-#include <network/packets/connection_response_packet.hpp>
-
 #include <RakPeerInterface.h>
 #include <MessageIdentifiers.h>
 #include <BitStream.h>
@@ -20,10 +15,6 @@
 #include <random>
 
 using nlohmann::json;
-using MCPIRelay::network::packets::ServerInfoPacket;
-using MCPIRelay::network::packets::ConnectionStatusPacket;
-using MCPIRelay::network::packets::ConnectionRequestPacket;
-using MCPIRelay::network::packets::ConnectionResponsePacket;
 using MCPIRelay::Server;
 
 
@@ -130,68 +121,6 @@ void Server::disconnect_client(MCPIRelay::Client *client) {
 	this->clients.erase(client);
 }
 
-void Server::handle_upstream_packet(RakNet::Packet *packet) {
-	if (!packet) return;
-	if (packet->bitSize != 0) {
-		RakNet::BitStream stream(packet->data, BITS_TO_BYTES((int)packet->bitSize), true);
-
-		uint8_t packet_id;
-	 	stream.Read<uint8_t>(packet_id);
-	 	if(packet_id == ConnectionResponsePacket::packet_id) {
-	 		ConnectionResponsePacket connection;
-	 		connection.deserialize_body(&stream);
-	 		std::cout << "[SERVER]: Got token: " << connection.token << "\n";
-
-	 		for(const auto& pair : this->relay->servers) {
-	 			if((!pair.second->ready) && (pair.second->awaiting_token == connection.token)) {
-	 				std::cout << "[SERVER]: Matched with server " << pair.second->name << "\n";
-	 				pair.second->ready = true;
-	 				pair.second->temporary_peer = this->temporary_peer;
-					pair.second->temporary_guid = this->temporary_guid;
-
-					ConnectionStatusPacket accept;
-					accept.status = 1;
-					send_downstream_packet(accept);
-
-					this->relay->server_peers.erase(packet->guid);
-					delete this;
-					return;
-	 			}
-	 		}
-			ConnectionStatusPacket reject;
-			reject.status = 0;
-			send_downstream_packet(reject);
-			std::cout << "[SERVER]: Rejected token\n";
-
-			this->relay->server_peers.erase(packet->guid);
-			delete this;
-			return;
-	 	} else if(packet_id == ServerInfoPacket::packet_id) {
-	 		ServerInfoPacket server_info;
-	 		server_info.deserialize_body(&stream);
-
-	 		json server = json::parse(server_info.info.C_String());
-
-	 		if(this->relay->servers.count(server["name"]) == 0) {
-	 			std::cout << "Server not in the list.\n";
-
-	 			this->relay->server_peers.erase(packet->guid);
-				delete this;
-				return;
-	 		}
-
-
-	 		server["token_name"];
-
-	 		// Authorize
-	 		// if authorized then save it in the relay
-	 		// else delete self
-
-
-	 	}
-	}
-}
-
 template <typename T>
 void Server::send_downstream_packet(T &packet) {
 	// Relay > server
@@ -199,21 +128,4 @@ void Server::send_downstream_packet(T &packet) {
 	send_stream.Write<uint8_t>(packet.packet_id);
 	packet.serialize_body(&send_stream);
 	this->temporary_peer->Send(&send_stream, IMMEDIATE_PRIORITY, RELIABLE_ORDERED, 0, this->temporary_guid, false);
-}
-
-int64_t Server::generate_unique_token() {
-	std::random_device rd;
-	std::uniform_int_distribution<int64_t> dist(INT64_MIN, INT64_MAX);
-	while(true) {
-		int64_t token = dist(rd);
-		bool is_free = true;
-		for(const auto& pair : this->relay->servers) {
-			if((!pair.second->ready) && (pair.second->awaiting_token == token)) {
-				is_free = false;
-			}
-		}
-		if(is_free) {
-			return token;
-		}
-	}
 }
